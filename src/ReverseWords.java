2a98141fcde00fb984cb84b04b46a52687a6b22e
Richa Sondhi <richa.sondhi@ibm.com>
2022-04-25 12:29:55 +0200

    commit# Refactor code Test passed

--- src/ReverseWords.java
+++ src/ReverseWords.java
@@ -1,4 +1,4 @@
-
+import java.util.Arrays;
 
 public class ReverseWords {
     public static void main(String[] args) {
@@ -8,7 +8,15 @@ public class ReverseWords {
     }
 
     public static String reverseWords(final String original) {
-
+        System.out.println("input string is " + original);
+        String[] tokens = original.split("\\s");
+        System.out.println("tokens are " + Arrays.toString(tokens));
+        String tmp = original;
+        for (String token : tokens) {
+            tmp = tmp.replaceFirst(token, reverse(token));
+        }
+        System.out.println("final string is " + tmp);
+        return tmp;
     }
 
     public static String reverse(String s) {
